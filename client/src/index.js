8579c6e63691e4d9ac5e0bb4c60083b5a7a8b1c8
Nur Islam <nurislam0333@gmail.com>
2018-01-21 17:01:39 +0600

    install react-router-dom | defined App route in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+
+
+ReactDOM.render(
+    <Router>
+        <Route exact path='/' component={App} />
+    </Router>,
+    document.getElementById('root')
+);
 registerServiceWorker();
