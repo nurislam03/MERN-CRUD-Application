cd08934118319b8989b212ae0288c01fb0041185
Nur Islam <nurislam0333@gmail.com>
2018-01-22 00:29:43 +0600

    mongoose connection and Middlewares setup

--- server/app.js
+++ server/app.js
@@ -1,7 +1,36 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var port = 4200;
+var cors = require('cors');
 
+
+//Mongoose connection with mongodb
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/NewDB');
+    .then(() => {
+        console.log('Start');
+    })
+    .catch((err) => {
+        console.error('App starting error:', err.stack);
+        process.exit(1);
+    });
+
+// Required aplication specific custom router module
+var itemRouter = require('./src/routes/itemRouter');
+
+// Use middlewares to set view engine and post json data to the server
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use('/items', itemRouter);
+
+// Start the server
 app.listen(port, function() {
-    console.log('hello world');
-})
+    console.log('Server is running on Port: ', port);
+});
