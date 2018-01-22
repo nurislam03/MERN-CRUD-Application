b7e6fc48b56865a466e2bf546e1c468af57d0f77
Nur Islam <nurislam0333@gmail.com>
2018-01-23 00:36:49 +0600

    updateData done

--- client/src/components/ItemService.js
+++ client/src/components/ItemService.js
@@ -1,16 +1,33 @@
 import axios from 'axios';
 
 class ItemService {
+
     sendData(data) {
         axios.post('http://localhost:4200/items/add/post', {
             item: data
         })
         .then((response) => {
-            console.log(response);
+            this.setState({
+                items: response.data
+            })
         })
         .then((error) => {
             console.log(error);
         });
+    }
+
+    updateDate(data, id) {
+        axios.post('http://localhost:4200/items/update/'+id, {
+            item: data
+        })
+        .then((response) => {
+            this.setState({
+                items: response.data
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     }
 }
 
